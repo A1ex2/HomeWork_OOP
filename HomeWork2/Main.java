f4667112207adf62189dfbf2f939990a65af9bec
A1ex2 <32175773+A1ex2@users.noreply.github.com>
2017-10-04 13:52:46 +0300

    Triangle

--- HomeWork2/Main.java
+++ HomeWork2/Main.java
@@ -5,10 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		Triangle triangleOne = new Triangle(3,4,5);
 		double areaOne = triangleOne.calcArea();
-		System.out.println("Площадь первого треугольника " + areaOne);
+		System.out.println("Площадь первого треугольника  " + areaOne);
 		
 		Triangle triangleTwo = new Triangle(3.5,5.5,6);
 		double areaTwo = triangleTwo.calcArea();
-		System.out.println("Площадь первого треугольника " + areaTwo);
+		System.out.println("Площадь первого треугольника  " + areaTwo);
 	}
 }
